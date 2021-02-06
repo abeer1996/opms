5371d84e84699eaaab2f60c17b0dc4e3b25eaa02
abeer1996 <17103361@iubat.edu>
2021-02-07 02:28:12 +0600

    last commit 7th night

--- routes/web.php
+++ routes/web.php
@@ -20,53 +20,57 @@ Route::get('/', function () {
 
 
     // Login Routing
-    Route::get('/login','OPMS\UserController@login')->name('login');
-    Route::post('/login','OPMS\UserController@loginProcess')->name('login.process');
+        Route::get('/login','OPMS\UserController@login')->name('login');
+        Route::post('/login','OPMS\UserController@loginProcess')->name('login.process');
 
 
-    Route::group(['middleware'=>'auth'], function(){
+        Route::group(['middleware'=>'auth'], function(){
         
         Route::get('/admin', function(){
-            return view('dashboard');
+            return view('home');
         });
 
     // Logout Routing
-     Route::get('/logout','OPMS\UserController@logout')->name('logout');
+        Route::get('/logout','OPMS\UserController@logout')->name('logout');
+
+
+
+        Route::group(['namespace'=>'OPMS'], function(){
+
+
+        Route::get('/','HomeController@dashboard')->name('dashboard');
+    
+        // Employee Routing
+        Route::get('/employeeList','EmployeeController@EmployeeList')->name('employee.list');
+        Route::get('/employeeForm','EmployeeController@EmployeeForm')->name('employee.form');
+        Route::post('/employeeFormSubmit','EmployeeController@EmployeeFormSubmit')->name('employee.fromsubmit');
+        
+        // Manager Routing
+        Route::get('/managerList','EmployeeController@ManagerList')->name('manager.list');
+        Route::get('/managerForm','EmployeeController@ManagerForm')->name('manager.form');
+        Route::post('/managerFormSubmit','EmployeeController@ManagerFormSubmit')->name('manager.formsubmit');
+        
+        // Department Routing
+        Route::get('/departmentList','DepartmentController@departmentList')->name('department.list');
+        Route::get('/departmentForm','DepartmentController@departmentForm')->name('department.form');
+        Route::post('/departmentFormSubmit','DepartmentController@departmentFormSubmit')->name('department.formsubmit');
+        
+        // Project Routing
+        Route::get('/projectList','ProjectController@projectList')->name('project.list');
+        Route::get('/projectForm','ProjectController@projectForm')->name('project.form');
+        Route::post('/projectFormSubmit','ProjectController@projectFormSubmit')->name('project.formsubmit');
+        
+        // Assign Project
+        Route::get('/project/assignProjectList','ProjectController@assignProjectList')->name('assign.list');
+        Route::get('/project/assignProjectForm','ProjectController@assignProjectForm')->name('assign.projectform');
+    
+    });
+
+
     });
 
     
 
-    Route::group(['namespace'=>'OPMS','middleware'=>'auth'], function(){
-
-
-    Route::get('/','HomeController@dashboard')->name('dashboard');
-
-    // Employee Routing
-    Route::get('/employeeList','EmployeeController@EmployeeList')->name('employee.list');
-    Route::get('/employeeForm','EmployeeController@EmployeeForm')->name('employee.form');
-    Route::post('/employeeFormSubmit','EmployeeController@EmployeeFormSubmit')->name('employee.fromsubmit');
     
-    // Manager Routing
-    Route::get('/managerList','EmployeeController@ManagerList')->name('manager.list');
-    Route::get('/managerForm','EmployeeController@ManagerForm')->name('manager.form');
-    Route::post('/managerFormSubmit','EmployeeController@ManagerFormSubmit')->name('manager.formsubmit');
-    
-    // Department Routing
-    Route::get('/departmentList','DepartmentController@departmentList')->name('department.list');
-    Route::get('/departmentForm','DepartmentController@departmentForm')->name('department.form');
-    Route::post('/departmentFormSubmit','DepartmentController@departmentFormSubmit')->name('department.formsubmit');
-    
-    // Project Routing
-    Route::get('/projectList','ProjectController@projectList')->name('project.list');
-    Route::get('/projectForm','ProjectController@projectForm')->name('project.form');
-    Route::post('/projectFormSubmit','ProjectController@projectFormSubmit')->name('project.formsubmit');
-    
-    // Assign Project
-    Route::get('/project/assignProjectList','ProjectController@assignProjectList')->name('assign.list');
-    Route::get('/project/assignProjectForm','ProjectController@assignProjectForm')->name('assign.projectform');
-
-
-
-});
 
 
